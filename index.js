4e574196bf9885520998aeebfa2460ea5addfdd9
MRinshad <rinshadmorayur09@gmail.com>
2025-01-28 12:26:41 +0530

    added console

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+    const clientIP = req.socket.remoteAddress;
+    console.log(`New request received from: ${clientIP}`);
+    console.log(`Request URL: ${req.url}`);
+    console.log(`Request Method: ${req.method}`);
+    
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World! Server is working.');
 });
